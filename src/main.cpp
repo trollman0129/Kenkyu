91d2d800b629357ad0e4f84f3bb3e0f3221b1c39
トロを <trollmann0129@Gmail.com>
2025-07-11 10:24:25 +0900

    hello github
    new

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+// #pragma once
+# pragma comment(lib, "portaudio_x86.lib")
+
+// #include "wave.h"
+#include "portaudio.h"
+
+#include <iostream>
+
+int main(){
+    char path[64] = "test_wav/test.wav";
+    
+    printf("Hello");
+
+    return 0;
+}
